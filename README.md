130831693901abbb618b49007a366d3cde7650aa
Dario Vladovic <d.vladimyr@gmail.com>
2019-05-07 16:32:30 +0200

    Implement standalone cli for `pm2-deploy`
    - reading deploymnet configuration from `.deployrc` (json),
      `.deployrc.js` or `deploy.toml`
    - implementing branch and remote fallback
    - predefining post deploy action

---
+++ README.md
@@ -0,0 +1,36 @@
+# deploy2-cli 
+[![build status](https://badgen.net/travis/vladimyr/deploy2-cli/master)](https://travis-ci.com/vladimyr/deploy2-cli) [![install size](https://badgen.net/packagephobia/install/{{name})](https://packagephobia.now.sh/result?p={{name}) [![npm package version](https://badgen.net/npm/v/deploy2-cli)](https://npm.im/deploy2-cli) [![github license](https://badgen.net/github/license/vladimyr/deploy2-cli)](https://github.com/vladimyr/deploy2-cli/blob/master/LICENSE) [![js semistandard style](https://badgen.net/badge/code%20style/semistandard/pink)](https://github.com/Flet/semistandard)
+
+> Deploy apps using [`pm2-deploy`](https://github.com/Unitech/pm2-deploy) :rocket:
+
+## Run
+```
+$ npx vladimyr/deploy2-cli <env> <command>
+```
+
+## Usage
+```
+$ npx vladimyr/deploy2-cli --help
+
+deploy2-cli v0.0.0 - Standalone CLI for PM2 deployment system
+
+Usage
+  $ deploy2-cli <env> <command>
+
+Commands
+  setup            run remote setup commands
+  revert [n]       revert to [n]th last deployment or 1
+  config [key]     output config file or [key]
+  curr[ent]        output current release commit
+  prev[ious]       output previous release commit
+  exec|run <cmd>   execute the given <cmd>
+  list             list previous deploy commits
+  ref [ref]        deploy [ref]
+
+Options
+  -h, --help       Show help
+  -v, --version    Show version number
+
+Homepage:     https://github.com/vladimyr/deploy2-cli
+Report issue: https://github.com/vladimyr/deploy2-cli/issues
+```
---
+++ cli.js
@@ -0,0 +1,162 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { format, promisify } = require('util');
+const { readFileSync } = require('fs');
+const git = require('git-rev-sync');
+const kleur = require('kleur');
+const logSymbols = require('log-symbols');
+const meow = require('meow');
+const deploy = promisify(require('pm2-deploy').deployForEnv);
+const JoyCon = require('joycon');
+const path = require('path');
+const pkg = require('./package.json');
+
+const stripJsonComments = require('strip-json-comments');
+const toml = require('toml');
+
+const CONFIG_FILES = ['.deploy', '.deployrc.js', 'deploy.toml'];
+const SHELL_OP_AND = '&&';
+
+const isValidationError = err => err.constructor.name === 'ValidationError';
+const formatCode = arg => `\`${arg}\``;
+const formatError = msg => msg.replace(/^\w*Error:\s+/, match => kleur.red().bold(match));
+const noop = () => {};
+const parseJSON = str => JSON.parse(stripJsonComments(str));
+
+const logError = (msg, ...args) => console.error(logSymbols.error, formatError(format(msg, ...args)));
+const logWarning = (msg, ...args) => console.error(logSymbols.warning, format(msg, ...args));
+const logSuccess = (msg, ...args) => console.error(logSymbols.success, format(msg, ...args));
+const logInfo = (msg, ...args) => console.error(logSymbols.info, format(msg, ...args));
+
+const joycon = new JoyCon({
+  files: CONFIG_FILES,
+  parseJSON
+});
+joycon.addLoader({
+  test: /^\.[^.]*rc$/,
+  loadSync: path => parseJSON(readFileSync(path, 'utf-8'))
+});
+joycon.addLoader({
+  test: /\.toml$/,
+  loadSync: path => toml.parse(readFileSync(path, 'utf-8'))
+});
+
+const commands = getCommands();
+const description = `${kleur.bold(pkg.name)} v${pkg.version} - ${pkg.description}`;
+const help = `
+Usage
+  $ ${pkg.name} <env> <command>
+
+Commands
+${showCommands()}
+
+Options
+  -h, --help       Show help
+  -v, --version    Show version number
+
+Homepage:     ${kleur.cyan(pkg.homepage)}
+Report issue: ${kleur.cyan(pkg.bugs.url)}`;
+
+const flags = {
+  help: { alias: 'h' },
+  version: { alias: 'v' }
+};
+
+program(meow(help, { description, flags }))
+  .then(() => logSuccess('Success'))
+  .catch(err => {
+    if (isValidationError(err)) {
+      logError('Error: Malformed configuration file.\n%s', err.message);
+      return process.exit(1);
+    }
+    logError(
+      'Error: %s\n%s',
+      err.message || 'Deploy error',
+      err.stack || ''
+    );
+    process.exit(1);
+  });
+
+function program(cli) {
+  const [env] = cli.input;
+  if (!env) {
+    logError('Error: Missing required argument: env');
+    cli.showHelp();
+    process.exit(1);
+  }
+
+  const { path: configPath, data: config } = joycon.loadSync();
+  if (!config) {
+    logError(
+      'Error: Configuration file not found.\n[Allowed file names: %s]',
+      CONFIG_FILES.join(', ')
+    );
+    process.exit(1);
+  }
+
+  const envConfig = config[env];
+  if (!config[env]) {
+    logError(
+      'Error: %s environment is not defined inside configuration file: %s',
+      kleur.cyan(env),
+      kleur.white(path.basename(configPath))
+    );
+    process.exit(1);
+  }
+
+  if (!envConfig.ref) {
+    const ref = git.branch();
+    logWarning(
+      '%s is not set, using current branch: %s',
+      kleur.white(formatCode(`${env}.ref`)),
+      kleur.white(JSON.stringify(ref))
+    );
+    envConfig.ref = ref;
+  }
+  if (!envConfig.repo) {
+    const repo = git.remoteUrl();
+    logWarning(
+      '%s is not set, using current remote: %s',
+      kleur.white(formatCode(`${env}.repo`)),
+      kleur.white(JSON.stringify(repo))
+    );
+    envConfig.repo = repo;
+  }
+
+  if (!envConfig['post-deploy']) {
+    envConfig['post-deploy'] = [
+      'npm install',
+      'npm run build',
+      'pm2 startOrRestart ecosystem.config.js'
+    ].join(` ${SHELL_OP_AND} `);
+  }
+
+  logInfo(
+    'Deploying to %s on host %s',
+    kleur.cyan(env),
+    kleur.cyan(envConfig.host)
+  );
+
+  const { log } = console.log;
+  console.log = noop;
+  return deploy(config, env, cli.input).finally(() => (console.log = log));
+}
+
+function showCommands(indent = '  ') {
+  const colWidth = Math.max(...commands.map(it => it.name.length), 16);
+  return commands.map(({ name, desc }) => {
+    return `${indent}${name.padEnd(colWidth)} ${desc}`;
+  }).join('\n');
+}
+
+function getCommands() {
+  const contents = readFileSync(require.resolve('pm2-deploy/deploy'), 'utf-8');
+  const lines = contents.split(/\r?\n/g);
+  return lines.slice(42, 50).map(line => {
+    line = line.trim();
+    const [name, desc] = line.split(/\s{2,}/);
+    return { name, desc };
+  });
+}
